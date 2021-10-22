64d9c50b2cbdbfbda681e88b34f130c93c04e267
Daniel MacLachlan <mackdan33@gmail.com>
2021-10-22 15:05:54 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace FoleyCommaDelimter
+{
+    partial class Foley
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoad.AutoSize = true;
+            this.btnLoad.Location = new System.Drawing.Point(350, 31);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(350, 2, 350, 2);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(100, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load CSV File";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btnLoad, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dgvCustomers, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.11111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80.88889F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCustomers.Location = new System.Drawing.Point(3, 88);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.Size = new System.Drawing.Size(794, 359);
+            this.dgvCustomers.TabIndex = 1;
+            // 
+            // Foley
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Foley";
+            this.Text = "Foley Comma Delimiter to Table";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgvCustomers;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace FoleyCommaDelimter
+{
+    public partial class Foley : Form
+    {
+        public class Customer
+        {
+            //basic get sets for the Customer class data
+            public string customerID    { get; set; }
+            public string firstName     { get; set; }
+            public string lastName      { get; set; }
+            public string address       { get; set; }
+            public void Output()
+            {
+                //output to table, table not made yet
+            }
+        }
+
+        //Object list for table output using foreach
+        public List<Customer> Customers = new List<Customer>();
+
+        public Foley()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                if (Path.GetExtension(file) != ".csv")
+                {
+                    MessageBox.Show("The file you have selected must be in .csv format", "Error with file format");
+                    return;
+                }
+                var fileContents = File.ReadAllLines(file);
+                //Removing first element (Headers)
+                fileContents = fileContents.Skip(0).ToArray();
+                try
+                {
+                    foreach(string i in fileContents)
+                    {
+                        //Regex to remove all commas that exist outside quotes
+                        List<string> customerInfo = Regex.Split(i, ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))").ToList();
+                        Customer Customer = new Customer();
+
+                        //Substrings for all except ID in order to trim leading and trailing quotes (checks if there is a quote first)
+                        Customer.customerID = customerInfo[0];
+
+                        //Firstname
+                        if(customerInfo[1].FirstOrDefault() == '"' && customerInfo[1].EndsWith("\""))
+                        {
+                            Customer.firstName = customerInfo[1].Substring(1, customerInfo[1].Length - 2);
+                        }
+                        else
+                        {
+                            Customer.firstName = customerInfo[1];
+                        }
+
+                        //Lastname
+                        if (customerInfo[2].FirstOrDefault() == '"' && customerInfo[2].EndsWith("\""))
+                        {
+                            Customer.lastName = customerInfo[2].Substring(1, customerInfo[2].Length - 2);
+                        }
+                        else
+                        {
+                            Customer.lastName = customerInfo[2];
+                        }
+
+                        //Address
+                        if (customerInfo[3].FirstOrDefault() == '"' && customerInfo[3].EndsWith("\""))
+                        {
+                            Customer.address = customerInfo[3].Substring(1, customerInfo[3].Length - 2);
+                        }
+                        else
+                        {
+                            Customer.address = customerInfo[3];
+                        }
+
+                        Customers.Add(Customer);
+                    }
+                    //Remove headers from object list
+                    Customers.RemoveAt(0);
+                    //Display list in data grid view
+                    dgvCustomers.DataSource = Customers;
+                    //Rename column headers
+                    dgvCustomers.Columns[0].HeaderText = "Customer ID";
+                    dgvCustomers.Columns[0].HeaderText = "First Name";
+                    dgvCustomers.Columns[0].HeaderText = "Last Name";
+                    dgvCustomers.Columns[0].HeaderText = "Address";
+
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("There was an unexpected error, please try again", "Unexpected Error");
+                    return;
+                }
+            }
+        }
+    }
+}
